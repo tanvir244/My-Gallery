b6f642ced8bfdb6a1919b97af7b9050b0a0a3149
tanvir243 <travelertanvir@gmail.com>
2024-03-11 03:24:58 +0600

    Album Project completed

--- albums.js
+++ albums.js
@@ -1,104 +1,123 @@
 const albums = [
   {
     id: 1,
-    img: 'https://images.unsplash.com/photo-1570201731886-7f21202470b7',
+    img: './images/1.jpeg',
     tag: 'Zurich, Switzerland',
+    description: 'It seems look like the canvas of artist'
   },
   {
     id: 2,
-    img: 'https://images.unsplash.com/photo-1590959914819-b767b9fe4cfb',
-    tag: 'Saudi arabia'
+    img: './images/2.jpeg',
+    tag: 'Saudi arabia',
+    description: 'Belong the holiest city in the world, called Makka.'
   },
   {
     id: 3,
-    img: 'https://images.unsplash.com/photo-1551009175-15bdf9dcb580',
-    tag: 'Canada'
+    img: './images/3.jpeg',
+    tag: 'Canada',
+    description: 'Land of ice burg'
   },
   {
     id: 4,
-    img: 'https://images.unsplash.com/photo-1610473254734-f22b13aef65b',
-    tag: 'Canada'
+    img: './images/4.jpeg',
+    tag: 'Canada',
+    description: 'Land of ice burg'
   },
   {
     id: 5,
-    img: 'https://images.unsplash.com/photo-1611223995703-1a1f6d9ebbc1',
-    tag: 'Saudi arabia'
+    img: './images/5.jpeg',
+    tag: 'Saudi arabia',
+    description: 'Belong the holiest city in the world, called Madina'
   },
   {
     id: 6,
-    img: 'https://images.unsplash.com/flagged/photo-1558113118-e42e558b352a',
-    tag: 'Pakistan'
+    img: './images/6.jpeg',
+    tag: 'Pakistan',
+    description: 'Paradise of delicious food'
   },
   {
     id: 7,
-    img: 'https://images.unsplash.com/photo-1603491656337-3b491147917c',
-    tag: 'Pakistan'
+    img: './images/7.jpeg',
+    tag: 'Pakistan',
+    description: 'Paradise of delicious food, most remarkable Karachi, Lahor and Islamabad'
   },
   {
     id: 8,
-    img: 'https://plus.unsplash.com/premium_photo-1675875488015-0ce54ed63552',
-    tag: 'Switzerland'
+    img: './images/8.jpeg',
+    tag: 'Switzerland',
+    description: 'Famous for Watch, Cheese and Chocolates'
   },
   {
     id: 9,
-    img: 'https://images.unsplash.com/photo-1601399681546-ff2983a355c3',
-    tag: 'Pakistan'
+    img: './images/9.jpeg',
+    tag: 'Pakistan',
+    description: 'Belong one of the most craziest city in the world'
   },
   {
     id: 10,
-    img: 'https://images.unsplash.com/photo-1508693926297-1d61ee3df82a',
-    tag: 'Canada'
+    img: './images/10.jpeg',
+    tag: 'Canada',
+    description: 'Land of ice burg'
   },
   {
     id: 11,
-    img: 'https://images.unsplash.com/photo-1503614472-8c93d56e92ce',
-    tag: 'Canada'
+    img: './images/11.jpeg',
+    tag: 'Canada',
+    description: 'Land of ice burg'
   },
   {
     id: 12,
-    img: 'https://images.unsplash.com/photo-1577609060534-4254158ea447',
-    tag: 'Malayasia'
+    img: './images/12.jpeg',
+    tag: 'Malayasia',
+    description: 'Famous for honesty of its people'
   },
   {
     id: 13,
-    img: 'https://images.unsplash.com/photo-1682687982093-4773cb0dbc2e',
-    tag: 'Saudi Arabia'
+    img: './images/13.jpeg',
+    tag: 'Saudi Arabia',
+    description: 'Massive memories attached with the prophets (S:)'
   },
   {
     id: 14,
-    img: 'https://images.unsplash.com/photo-1709874662602-139e1114da11',
-    tag: 'Nature, sky, sunset'
+    img: './images/14.jpeg',
+    tag: 'Nature, sky, sunset',
+    description: 'Exploring nature with lonely'
   },
   {
     id: 15,
-    img: 'https://images.unsplash.com/photo-1614862053138-10ca7a58866e',
-    tag: 'Antelope Canyon, Arizona, USA'
+    img: './images/15.jpeg',
+    tag: 'Antelope Canyon, Arizona, USA',
+    description: 'Land of immigrant, known for super power in politically'
   },
   {
     id: 16,
-    img: 'https://images.unsplash.com/photo-1484766280341-87861644c80d',
-    tag: 'Sunset'
+    img: './images/16.jpeg',
+    tag: 'Sunset',
+    description: 'The most enjoyable moment'
+
   },
   {
     id: 17,
-    img: 'https://plus.unsplash.com/premium_photo-1700577888169-fb5081ab5b1f',
-    tag: 'sunset'
+    img: './images/17.jpeg',
+    tag: 'sunset',
+    description: 'The most enjoyable moment'
   },
   {
     id: 18,
-    img: 'https://images.unsplash.com/photo-1521336575822-6da63fb45455',
-    tag: 'Gudvangen, Norway'
+    img: './images/18.jpeg',
+    tag: 'Gudvangen, Norway',
+    description: "Very beautiful and peaceful country"
   },
   {
     id: 19,
-    img: 'https://images.unsplash.com/photo-1505845664900-f883fde76fb0',
-    tag: 'Waimea, United States'
+    img: './images/19.jpeg',
+    tag: 'Waimea, United States',
+    description: 'North Amarican country'
   },
   {
     id: 20,
-    img: 'https://images.unsplash.com/photo-1519865885898-a54a6f2c7eea',
-    tag: 'Laguna Beach, United States'
+    img: './images/20.jpeg',
+    tag: 'Laguna Beach, United States',
+    description: 'North Amarican country'
   }
 ];
-
-
--- index.js
+++ index.js
@@ -1,22 +1,59 @@
-console.log(albums)
 
-function showGalleryPhoto() {
+
+for(const album of albums){
+    showGalleryPhoto(album);
+}
+
+function showGalleryPhoto(albm) {
+    const {img, tag, description} = albm;
+
     const card = document.createElement('div');
     card.className = 'card relative';
 
-    const img = document.createElement('img');
-    img.src = 'https://images.unsplash.com/photo-1678729466198-a34ccca023c3';
+    const image = document.createElement('img');
+    image.className = 'h-[251px] w-full'
+    image.src = img;
 
     const intro = document.createElement('div');
     intro.className = 'intro';
 
-    const p = document.createElement('p');
-    p.className = 'text-white font-semibold';
-    p.innerText = 'Tag';
-    intro.appendChild(p);
+    const h3 = document.createElement('h3');
+    h3.className = 'text-white text-xl font-semibold';
+    h3.innerText = tag;
 
-    card.append(img, intro);
+    const p1 = document.createElement('p');
+    p1.className = 'text-white text-xs';
+    p1.innerText = description;
+
+    intro.append(h3, p1);
+
+    card.append(image, intro);
 
     document.querySelector('.gallary-container').append(card);
 }
-showGalleryPhoto();
+
+document.querySelector('#search-input').
+addEventListener('keyup', function(e) {
+    const keyword = e.target.value.toLowerCase();
+    const card = document.querySelectorAll('.card');
+
+    let notFound = true;
+
+    for(const element of card){
+        const tag = element.children[0].innerText.toLowerCase();
+        const description = element.children[1].innerText.toLowerCase();
+        if(tag.includes(keyword) || description.includes(keyword)){
+            element.style.display = 'block';
+            notFound = false;
+        }else{
+            element.style.display = 'none';
+        }
+
+        if(notFound){
+            document.querySelector('.not-found').style.display = 'block';
+        }else {
+            document.querySelector('.not-found').style.display = 'none';
+        }
+
+    }
+})
