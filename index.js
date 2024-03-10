9a350e77ed498ba199d7405c9678eb3363e25e5e
tanvir243 <travelertanvir@gmail.com>
2024-03-10 21:41:57 +0600

    showing my gallery photo

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+import { albums } from "./albums";
+console.log(albums);
 
+function showGalleryPhoto() {
+    const card = document.createElement('div');
+    card.className = 'card relative';
+
+    const img = document.createElement('img');
+    img.src = 'https://images.unsplash.com/photo-1678729466198-a34ccca023c3';
+
+    const intro = document.createElement('div');
+    intro.className = 'intro';
+
+    const p = document.createElement('p');
+    p.className = 'text-white font-semibold';
+    p.innerText = 'Tag';
+    intro.appendChild(p);
+
+    card.append(img, intro);
+
+    document.querySelector('.gallary-container').append(card);
+}
