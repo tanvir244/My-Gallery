134b0423a22f422b08720ac50794f1c52e12a688
tanvir243 <travelertanvir@gmail.com>
2024-03-10 20:25:42 +0600

    My Gallery design complete

---
+++ albums.js
@@ -0,0 +1,102 @@
+export const albums = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1570201731886-7f21202470b7',
+    tag: 'Zurich, Switzerland',
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1590959914819-b767b9fe4cfb',
+    tag: 'Saudi arabia'
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1551009175-15bdf9dcb580',
+    tag: 'Canada'
+  },
+  {
+    id: 4,
+    img: 'https://images.unsplash.com/photo-1610473254734-f22b13aef65b',
+    tag: 'Canada'
+  },
+  {
+    id: 5,
+    img: 'https://images.unsplash.com/photo-1611223995703-1a1f6d9ebbc1',
+    tag: 'Saudi arabia'
+  },
+  {
+    id: 6,
+    img: 'https://images.unsplash.com/flagged/photo-1558113118-e42e558b352a',
+    tag: 'Pakistan'
+  },
+  {
+    id: 7,
+    img: 'https://images.unsplash.com/photo-1603491656337-3b491147917c',
+    tag: 'Pakistan'
+  },
+  {
+    id: 8,
+    img: 'https://plus.unsplash.com/premium_photo-1675875488015-0ce54ed63552',
+    tag: 'Switzerland'
+  },
+  {
+    id: 9,
+    img: 'https://images.unsplash.com/photo-1601399681546-ff2983a355c3',
+    tag: 'Pakistan'
+  },
+  {
+    id: 10,
+    img: 'https://images.unsplash.com/photo-1508693926297-1d61ee3df82a',
+    tag: 'Canada'
+  },
+  {
+    id: 11,
+    img: 'https://images.unsplash.com/photo-1503614472-8c93d56e92ce',
+    tag: 'Canada'
+  },
+  {
+    id: 12,
+    img: 'https://images.unsplash.com/photo-1577609060534-4254158ea447',
+    tag: 'Malayasia'
+  },
+  {
+    id: 13,
+    img: 'https://images.unsplash.com/photo-1682687982093-4773cb0dbc2e',
+    tag: 'Saudi Arabia'
+  },
+  {
+    id: 14,
+    img: 'https://images.unsplash.com/photo-1709874662602-139e1114da11',
+    tag: 'Nature, sky, sunset'
+  },
+  {
+    id: 15,
+    img: 'https://images.unsplash.com/photo-1614862053138-10ca7a58866e',
+    tag: 'Antelope Canyon, Arizona, USA'
+  },
+  {
+    id: 16,
+    img: 'https://images.unsplash.com/photo-1484766280341-87861644c80d',
+    tag: 'Sunset'
+  },
+  {
+    id: 17,
+    img: 'https://plus.unsplash.com/premium_photo-1700577888169-fb5081ab5b1f',
+    tag: 'sunset'
+  },
+  {
+    id: 18,
+    img: 'https://images.unsplash.com/photo-1521336575822-6da63fb45455',
+    tag: 'Gudvangen, Norway'
+  },
+  {
+    id: 19,
+    img: 'https://images.unsplash.com/photo-1505845664900-f883fde76fb0',
+    tag: 'Waimea, United States'
+  },
+  {
+    id: 20,
+    img: 'https://images.unsplash.com/photo-1519865885898-a54a6f2c7eea',
+    tag: 'Laguna Beach, United States'
+  }
+];
